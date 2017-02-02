6aed20b83e26a6d1a6c657b91647cf8786fd6bb3
Silvia <silvia.quintanar.robles@gmail.com>
2017-02-02 11:59:38 +0100

    Añadido metodo addCarta()

--- Baza.java
+++ Baza.java
@@ -8,7 +8,9 @@
 public class Baza
 {
     // instance variables - replace the example below with your own
-    
+    private Carta baza[];
+	private String nombresJugadores[];
+	private int contadorTiradas;
 
     /**
      * Constructor for objects of class Baza
@@ -16,8 +18,19 @@ public class Baza
     public Baza(int numeroJugadores, int paloQuePinta)
     {
         // initialise instance variables
-        
+        baza = new Carta[numeroJugadores];
+		nombresJugadores = new String[numeroJugadores];
+		contadorTiradas = 0;
     }
 
-   
+    public void addCarta(Carta cartaTirada, String nombreJugador)
+    {	
+        while(contadorTiradas < baza.length){
+			baza[contadorTiradas] = cartaTirada;
+			nombresJugadores[contadorTiradas] = nombreJugador;
+			contadorTiradas++;
+		}
+    }
+
+	
 }
