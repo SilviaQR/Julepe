69fa6093cdb40ef09982e083d2f1e4f8865c5e75
Silvia <silvia.quintanar.robles@gmail.com>
2017-02-02 12:44:59 +0100

    Añadido el metodo cartaQueVaGanandoLaBaza()

--- Baza.java
+++ Baza.java
@@ -9,8 +9,9 @@ public class Baza
 {
     // instance variables - replace the example below with your own
     private Carta baza[];
-	private String nombresJugadores[];
-	private int contadorTiradas;
+    private String nombresJugadores[];
+    private int contadorTiradas;
+    private int paloQuePinta;
 
     /**
      * Constructor for objects of class Baza
@@ -19,26 +20,46 @@ public class Baza
     {
         // initialise instance variables
         baza = new Carta[numeroJugadores];
-		nombresJugadores = new String[numeroJugadores];
-		contadorTiradas = 0;
+        nombresJugadores = new String[numeroJugadores];
+        contadorTiradas = 0;
+        this.paloQuePinta = paloQuePinta;
     }
 
     public void addCarta(Carta cartaTirada, String nombreJugador)
-    {	
+    {   
         while(contadorTiradas < baza.length){
-			baza[contadorTiradas] = cartaTirada;
-			nombresJugadores[contadorTiradas] = nombreJugador;
-			contadorTiradas++;
-		}
+            baza[contadorTiradas] = cartaTirada;
+            nombresJugadores[contadorTiradas] = nombreJugador;
+            contadorTiradas++;
+        }
     }
 
-	public int getPaloPrimeraCartaDeLaBaza()
-	{
-		int posicion = -1;
-		posicion = baza[0].getPalo();
-		return posicion;
-	}
+    public int getPaloPrimeraCartaDeLaBaza()
+    {
+        int posicion = -1;
+        posicion = baza[0].getPalo();
+        return posicion;
+    }
 
-    public void cartaQueVaGanandoLaBaza()
-	{}
+    public Carta cartaQueVaGanandoLaBaza()
+    {
+        Carta cartaQueGana = null;
+        int index = 0;
+        if(baza[index] != null){
+            cartaQueGana = baza[index];
+            index++;
+            while(index < baza.length){
+                if(cartaQueGana.ganaA(baza[index], paloQuePinta)){
+                    cartaQueGana = baza[index];
+                    index++;
+                }
+                index++;
+            }   
+        }
+        return cartaQueGana;
+        }
+        
+        
+       
 }
+
